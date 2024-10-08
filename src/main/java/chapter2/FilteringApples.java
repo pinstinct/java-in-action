119d56206466081f6c6c1547db98e8ae3ca09f56
pinstinct <contact@limhm.dev>
2024-10-08 13:19:51 +0900

    프레디케이트와 전략 디자인 패턴

--- src/main/java/chapter2/FilteringApples.java
+++ src/main/java/chapter2/FilteringApples.java
@@ -39,4 +39,30 @@ public class FilteringApples {
     }
     return result;
   }
+
+  /**
+   * 전략 디자인 패턴: 각 알고리즘(전략이라 불리는)을 캡슐화하는 알고리즘 패밀리를 정의해둔 다음에 런타임에 알고리즘을 선택하는 기법이다.
+   * {@code ApplePredicate}가 알고리즘 패밀리고, {@code AppleHeavyWeightPredicate}, {@code AppleGreenColorPredicate}가 전략이다.
+   */
+  public interface ApplePredicate {  // 사과 선택 전략을 캡슐화함
+
+    boolean test(Apple apple);
+  }
+
+  static class AppleHeavyWeightPredicate implements ApplePredicate {  // 무거운 사과만 선택
+
+    @Override
+    public boolean test(Apple apple) {
+      return apple.getWeight() > 150;
+    }
+  }
+
+  static class AppleGreenColorPredicate implements ApplePredicate {  // 녹색 사과만 선택
+
+    @Override
+    public boolean test(Apple apple) {
+      return GREEN.equals(apple.getColor());
+    }
+
+  }
 }
