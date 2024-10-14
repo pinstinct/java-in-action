4e550ad79f99d7646638e28f6211e96577c4d5d1
pinstinct <contact@limhm.dev>
2024-10-14 14:41:36 +0900

    슬라이싱

--- src/test/java/chapter5/StreamTest.java
+++ src/test/java/chapter5/StreamTest.java
@@ -21,7 +21,7 @@ public class StreamTest {
   public class Filtering {
 
     @Test
-    @DisplayName("filter")
+    @DisplayName("predicate")
     void test1() {
       List<Dish> vegetarianMenu = menus.stream()
           .filter(Dish::isVegetarian)
@@ -37,6 +37,68 @@ public class StreamTest {
           .filter(i -> i % 2 == 0)
           .distinct()
           .forEach(System.out::println);
+    }
+  }
+
+  @Nested
+  @DisplayName("스트림 슬라이싱")
+  class Slicing {
+
+    // 칼로리 순으로 정렬된 리스트
+    List<Dish> specialMenu = Arrays.asList(
+        new Dish("seasonal fruit", true, 120, Type.OTHER),
+        new Dish("prawns", false, 300, Type.FISH),
+        new Dish("rice", true, 350, Type.OTHER),
+        new Dish("chicken", false, 400, Type.MEAT),
+        new Dish("french fries", true, 530, Type.OTHER)
+    );
+
+    @Test
+    @DisplayName("predicate - takeWhile")
+    void test1() {
+      List<Dish> sliceMenu = specialMenu.stream()
+          .takeWhile(dish -> dish.getCalories() < 320)
+          .toList();
+      System.out.println(sliceMenu);
+    }
+
+    @Test
+    @DisplayName("predicate - dropWhile")
+    void test2() {
+      List<Dish> dishes = specialMenu.stream()
+          .dropWhile(dish -> dish.getCalories() < 320)  // 거짓이 되는 지점에서 작업 중단하고, 발견된 요소를 버린다.
+          .toList();
+      System.out.println(dishes);
+    }
+
+    @Test
+    @DisplayName("스트림 축소")
+    void test3() {
+      List<Dish> dishes = specialMenu.stream()
+          .filter(dish -> dish.getCalories() > 300)
+          .limit(3)
+          .toList();
+      System.out.println(dishes);
+    }
+
+    @Test
+    @DisplayName("요소 건너뛰기")
+    void test4() {
+      List<Dish> dishes = menus.stream()
+          .filter(dish -> dish.getCalories() > 300)
+          .skip(2)  // 처음 n개 요소를 제외한 스트림 반환
+          .toList();
+      System.out.println(dishes);
+    }
+
+    @Test
+    @DisplayName("처음 등장하는 두 고기 요리를 필터링")
+    void test5() {
+      List<Dish> dishes = menus.stream()
+          .filter(menu -> menu.getType().equals(Type.MEAT))
+          .limit(2)
+          .toList();
+      System.out.println(dishes);
     }
   }
 }
