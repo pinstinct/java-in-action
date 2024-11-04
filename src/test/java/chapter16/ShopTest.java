e83b239674d1ff2f3918bdfb256d45e0b54b3fe5
pinstinct <contact@limhm.dev>
2024-11-04 10:09:55 +0900

    비동기 코드 만들기 & 커스텀 Executor 사용하기 테스트 코드 정리

--- src/test/java/chapter16/ShopTest.java
+++ src/test/java/chapter16/ShopTest.java
@@ -6,49 +6,54 @@ import static chapter16.Shop.findPricesParallel;
 import static chapter16.Shop.findPricesWithCF;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("비블록 코드 만들기")
 class ShopTest {
 
-  @Test
-  @DisplayName("findPrices 결과와 성능 확인")
-  void test1() {
-    long start = System.nanoTime();
-    // 원하는 제품의 가격을 검색
-    System.out.println(findPrices("myPhone27s"));
-    long duration = (System.nanoTime() - start) / 1_000_000;
-    System.out.println("Done in " + duration + "m sec");
-  }
+  @Nested
+  @DisplayName("비블록 코드 만들기")
+  class NonBlockTest {
 
-  @Test
-  @DisplayName("병렬 스트림으로 요청 병렬화하기")
-  void test2() {
-    long start = System.nanoTime();
-    // 원하는 제품의 가격을 검색
-    System.out.println(findPricesParallel("myPhone27s"));
-    long duration = (System.nanoTime() - start) / 1_000_000;
-    System.out.println("Done in " + duration + "m sec");
-    System.out.println(Runtime.getRuntime().availableProcessors());
-  }
+    @Test
+    @DisplayName("findPrices 결과와 성능 확인")
+    void test1() {
+      long start = System.nanoTime();
+      // 원하는 제품의 가격을 검색
+      System.out.println(findPrices("myPhone27s"));
+      long duration = (System.nanoTime() - start) / 1_000_000;
+      System.out.println("Done in " + duration + "m sec");
+    }
 
-  @Test
-  @DisplayName("CompletableFuture로 비동기 호출 구현하기")
-  void test3() {
-    long start = System.nanoTime();
-    // 원하는 제품의 가격을 검색
-    System.out.println(findPricesWithCF("myPhone27s"));
-    long duration = (System.nanoTime() - start) / 1_000_000;
-    System.out.println("Done in " + duration + "m sec");
-  }
+    @Test
+    @DisplayName("병렬 스트림으로 요청 병렬화하기")
+    void test2() {
+      long start = System.nanoTime();
+      // 원하는 제품의 가격을 검색
+      System.out.println(findPricesParallel("myPhone27s"));
+      long duration = (System.nanoTime() - start) / 1_000_000;
+      System.out.println("Done in " + duration + "m sec");
+      System.out.println(Runtime.getRuntime().availableProcessors());
+    }
 
-  @Test
-  @DisplayName("커스텀 Executor 사용하기")
-  void test4() {
-    long start = System.nanoTime();
-    // 원하는 제품의 가격을 검색
-    System.out.println(findPriceWithExecutor("myPhone27s"));
-    long duration = (System.nanoTime() - start) / 1_000_000;
-    System.out.println("Done in " + duration + "m sec");
+    @Test
+    @DisplayName("CompletableFuture로 비동기 호출 구현하기")
+    void test3() {
+      long start = System.nanoTime();
+      // 원하는 제품의 가격을 검색
+      System.out.println(findPricesWithCF("myPhone27s"));
+      long duration = (System.nanoTime() - start) / 1_000_000;
+      System.out.println("Done in " + duration + "m sec");
+    }
+
+    @Test
+    @DisplayName("커스텀 Executor 사용하기")
+    void test4() {
+      long start = System.nanoTime();
+      // 원하는 제품의 가격을 검색
+      System.out.println(findPriceWithExecutor("myPhone27s"));
+      long duration = (System.nanoTime() - start) / 1_000_000;
+      System.out.println("Done in " + duration + "m sec");
+    }
   }
 }
